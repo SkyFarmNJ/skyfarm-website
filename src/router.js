47b01404ad796b1d3f377a5575669dc1d27f5ee7
Dan Speers <dspeers@collab.net>
2020-08-19 22:00:28 -0400

    New Membership page.

--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter  from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue'
-import AboutUs from './components/AboutUs.vue'
+import AboutUs    from './components/AboutUs.vue'
 import MemberPage from './components/MemberPage.vue'
+import Membership from './components/Membership.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/', component: HelloWorld},
-    { path: '/about', component: AboutUs},
-    { path: '/docs', component: MemberPage}
+    { path: '/',           component: HelloWorld},
+    { path: '/about',      component: AboutUs   },
+    { path: '/docs',       component: MemberPage},
+    { path: '/membership', component: Membership}
 ]
 
 export default new VueRouter({routes})
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import HelloWorld from './components/HelloWorld.vue'
-import AboutUs from './components/AboutUs.vue'
+import AboutUs    from './components/AboutUs.vue'
 import MemberPage from './components/MemberPage.vue'
+import Membership from './components/Membership.vue'
 
 const routes = [
     { path: '/', component: HelloWorld},
     { path: '/about', component: AboutUs},
     { path: '/docs', component: MemberPage}
+    { path: '/membership', component: Membership},
 ]
 
 export default routes;
