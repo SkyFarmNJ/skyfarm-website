8574e3da53359ead2abc4a6268c5706dd81a4f8e
Dan Speers <dspeers@collab.net>
2020-08-21 17:18:56 -0400

    About and Membership updates

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container mx-10 fluid>
     <v-row class="text-center">
       <v-col cols="12" mb-5>
         <v-btn>Schedule a private tour of Sky Farm!</v-btn>
@@ -9,20 +9,33 @@
         <h2>Sky Farm is New Jersey's best kept secret!</h2>
         <p class="ma-10">Sky Farm is America’s oldest naturist recreational community. Sitting on 36 acres of lush trees and woods at the base of the Watchung Mountains in Basking Ridge, New Jersey, individuals, couples and families have been enjoying nature naturally here since 1932.</p>
       </v-col>
+    </v-row>
 
-      <v-col cols="4"
-             mb-5
-             v-for="(pic, i) in pics"
-             :key=i>
+    <v-row class="text-center">
+      <v-col cols="4" mb-5>
         <v-img
-          :src=pic
+          src="../assets/2019.jpg"
           contain
           height="200"/>
       </v-col>
+      <v-col cols="4" mb-5>
+	<p>Got questions? We have answers. Check out our FAQ!</P>
 
+	<p>Learn more about Sky Farm’s history!</P>
+
+      </v-col>
+      <v-col cols="4" mb-5>
+        <v-img
+          src="../assets/couple2final.jpg"
+          contain
+          height="200"/>
+      </v-col>
+    </v-row>
+
+    <v-row class="text-left">
       <v-col cols="12" mb-5>
         <h3>But wait! There's more!</h3>
-
+	<ul>
         <li>At Sky Farm, responsible drinking of adult beverages is permitted. (No glass at the pool or hot tub!) Fill your cup or cooler at the ice machine!</li>
         <li>Our clubhouse has a full kitchen and event space — perfect for weekend lunches and super-fun events all season long!</li>
         <li>Check out Sky Farm’s calendar of events! You’ll find countless recreational activities including swimming, petanque, shuffleboard, pop tennis, volleyball and much more. The clubhouse, open 24/7 to members, has a pool table, poker table, foosball, dart board, a Big Screen TV and free WiFi.</li>
@@ -31,6 +44,7 @@
         <li>Hygiene is a top priority at Sky Farm. Showers are required before using the pool, hot tub or sauna. </li>
         <li>Lodge rooms and tent sites available for members to rent at nominal rates</li>
         <li>Sky Farm is an AANR-affiliated facility</li>
+	</ul>
       </v-col>
 
     </v-row>
--- src/components/Membership.vue
+++ src/components/Membership.vue
@@ -1,13 +1,25 @@
 <template>
-  <v-container fluid>
-    <v-row class="text-center">
-      <v-col cols="12" mb-5>
+  <v-container mx-10 fluid>
+    <v-row class="text-left">
+      <v-col cols="12" mb-5 class="text-center">
         <v-btn>Schedule a private tour of Sky Farm!</v-btn>
       </v-col>
       <v-col cols="12" mb-5>
         <h2>Sky Farm Welcomes New Members!</h2>
-        <p class="ma-10">Do you already know the joys of being a naturist? Or, have always dreamed of skinny dipping in a crystal clear pool, relaxing in a steamy hot tub and hanging out with great people au naturel? </p>
-	<p class="ma-10">Either way, Sky Farm is the place for you!</p>
+        <p class="ma-1">Do you already know the joys of being a naturist? Or, have always dreamed of skinny dipping in a crystal clear pool, relaxing in a steamy hot tub and hanging out with great people au naturel? </p>
+	<p class="ma-1">Either way, Sky Farm is the place for you!</p>
+      </v-col>
+
+      <v-col cols="12"  justify="left">
+	<ul class="ma-2">
+	  <li><b>Relax!</b> Sky Farm is a family-friendly nudist membership resort. Be yourself — in your birthday suit! </li>
+	  <li><b>Get involved!</b> There are tons of activities: 24/7 access to pool, hot tub & sauna, sports and games,  parties and dances, dinners and special events like Friendsgiving, Halloween Party, Pub & Poker Nights…. The list goes on and on.</li>
+	  <li>Enjoy Sky Farm’s beautiful grounds, lodge, clubhouse, immaculate bathrooms and showers and more!</li>
+	</ul>
+	<p>The Sky Farm community is a member-owned cooperative with approximately 60 residential cabins. Our gorgeous pool and invigorating hot tub are enjoyed by members 24/7 from May to October.</p>
+	<p>You must be a member to use Sky Farm’s beautiful facilities. We do not offer day passes.</p>
+        <p>With a full calendar of events from pub nights and potlucks to karaoke and DJ-dances to poker nights to sports galore, there is truly something for everyone at Sky Farm.</p>
+        <p>Note: Memberships are limited and everyone is probationary for a minimum of one year. Sky Farm strive for a balance between couples and singles as well as males and females, so there can sometimes be a waiting list.</p>
       </v-col>
 
       <v-col cols="4"
@@ -20,18 +32,6 @@
           height="200"/>
       </v-col>
 
-      <v-col cols="12" mb-5>
-        <h3>But wait! There's more!</h3>
-
-        <li>At Sky Farm, responsible drinking of adult beverages is permitted. (No glass at the pool or hot tub!) Fill your cup or cooler at the ice machine!</li>
-        <li>Our clubhouse has a full kitchen and event space — perfect for weekend lunches and super-fun events all season long!</li>
-        <li>Check out Sky Farm’s calendar of events! You’ll find countless recreational activities including swimming, petanque, shuffleboard, pop tennis, volleyball and much more. The clubhouse, open 24/7 to members, has a pool table, poker table, foosball, dart board, a Big Screen TV and free WiFi.</li>
-        <li>Looking for more serene activities? Take a long walk through the lush greens, settle in with a good book from our library or get your hands dirty in our community vegetable garden.</li>
-        <li>Keep your chairs and umbrellas onsite, near the pool — no hassles here!</li>
-        <li>Hygiene is a top priority at Sky Farm. Showers are required before using the pool, hot tub or sauna. </li>
-        <li>Lodge rooms and tent sites available for members to rent at nominal rates</li>
-        <li>Sky Farm is an AANR-affiliated facility</li>
-      </v-col>
 
     </v-row>
   </v-container>
@@ -41,7 +41,7 @@
   export default {
       data: () => ({
           pics: [
-              require('../assets/2019.jpg'),
+              require('../assets/streetsignfinal.jpg'),
               require('../assets/hottubweb.jpg'),
               require('../assets/couple2final.jpg')
           ]
