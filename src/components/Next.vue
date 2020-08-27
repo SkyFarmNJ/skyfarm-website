e2034f10c8f8e9834b20d0082348e35236cf1f7e
Dan Speers <dspeers@collab.net>
2020-08-27 19:31:01 -0400

    Content updates

--- src/components/Next.vue
+++ src/components/Next.vue
@@ -13,13 +13,13 @@
         <p>Your tour request has been sent to our Membership team for review.  Soon after you will be contacted via phone or email to schedule your tour.</p>
 
         <h3>What to Expect</h3>
-        <p>Upon arrival a membership committee representative will meet you at the gate.  They will acquaint you with the rules of Sky Farm, show around the grounds, introduce to other members and answer any questions you may have.</p>
+        <p>Upon arrival a membership committee representative will meet you at the gate.  They will acquaint you with the rules of Sky Farm, show you around the grounds, introduce you to other members and answer any questions you may have.</p>
 
         <h3>What should I bring?</h3>
         <p>Towels, ID, sunscreen, shoes, snacks and anything else you need to feel comfortable.<p>
 
         <h3>So what is next?</h3>
-        <p>Relax and wait for an email or phone call.  Also check out our FAQ page for more.</p>
+        <p>Relax and wait for an email or phone call.  Also check out our FAQ page for more information.</p>
 
       </v-col>
     </v-row>
